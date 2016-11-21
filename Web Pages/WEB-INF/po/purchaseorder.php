445c7158bcb903fad70b3a5666dcc5282afe2a9f
Gina Co <gina_co@dlsu.edu.ph>
2016-11-21 23:59:06 +0800

    fixed

--- Web Pages/WEB-INF/po/purchaseorder.php
+++ Web Pages/WEB-INF/po/purchaseorder.php
@@ -15,96 +15,120 @@
 
 </head>
 <body>
-<div class="app app-header-fixed ">
-  
+  <div class="app app-header-fixed ">
+
 
    <!-- nav -->
    <?php include '../session/levelOfAccess.php';?>
    
    <!-- / nav -->
 
-<?php
+   <?php
    if ($_SESSION['usertype']!=102){
     header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."../../accounts/login.php");
   }
+  require_once('../../mysqlConnector/mysql_connect.php');
+                $query="select productID, productName, wholesalePrice, retailPrice, sku from products";
+                $result=mysqli_query($dbc,$query);
   ?>
 
   <!-- content -->
   <div id="content" class="app-content" role="main">
   	<div class="app-content-body ">
-	    
 
-<div class="bg-light lter b-b wrapper-md">
-  <h1 class="m-n font-thin h3">Purchase Order</h1>
+
+      <div class="bg-light lter b-b wrapper-md">
+        <h1 class="m-n font-thin h3">Purchase Order</h1>
+      </div>
+      <div class="wrapper-md">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            Create Purchase Order
+          </div>
+          <div class="wrapper-md">
+
+           <b>Products to be Ordered
+           </b> <br>
+           <select id="productChosen">
+             <?php 
+                require_once('../../mysqlConnector/mysql_connect.php');
+                $query="select productID, productName, wholesalePrice, retailPrice, sku from products;";
+                $result=mysqli_query($dbc,$query);
+                
+                if ($result!=null){
+                  while($row=$result->fetch_assoc()) {
+                    echo"<option value=".$row["productID"].">".$row["productName"]."</option>";
+                  }
+                }
+                ?>
+           </select> <button type="submit" name="add" class="btn btn-default" id="add">Add Product</button><br>
+           <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <div class="table-responsive">
+              <table  class="table table-striped b-t b-b" id="myTable">
+                <thead>
+                  <tr>
+                    <th  style="width:25%">Product Name</th>
+                    <th  style="width:10%">Quantity</th>
+                    <th  style="width:10%"></th>
+
+                  </tr>
+                </thead>
+                <tbody id="tableList">
+               </tbody>
+              </table>
+              <button type="submit" name="confirm" class="btn btn-success">Submit</button> 
+              </form>
+              
+            </div>
+
 </div>
-<div class="wrapper-md">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      Create Purchase Order
-    </div>
-    <div class="wrapper-md">
-    
-     <b>Products to be Ordered
-        </b> 
+</div>
+</div>
+</div>
+</div>
+</div>
 
-    <div class="table-responsive">
-      <table  class="table table-striped b-t b-b">
-        <thead>
-          <tr>
-            <th  style="width:10%">SKU</th>
-            <th  style="width:25%">Product Name</th>
-            <th  style="width:10%">Quantity</th>
-            <th  style="width:10%">Purchasing Price</th>
-            <th  style="width:10%">Selling Price</th>
+<?php
+        if (isset($_POST['confirm'])){
+            $productID=$_POST['productName'];
+            $purchaseQty=$_POST['orderQty'];
+            $total="n/a";
             
-          </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <td>QSPT200</td>
-            <td>200g Quesong Puti</td>
-            <td><div class="col-sm-10">
-            <input type="number" min="0" ui-options="" class="form-control w-md" />
-          </div></td>
-            <td>Php 50.00</td>
-            <td>Php 70.00</td>
-          
-      
-          </tr>
-             <tr>
-            <td>CHMK500</td>
-            <td>500ml Chocolate Milk</td>
-            <td><div class="col-sm-10">
-            <input type="number" min="0" ui-options="" class="form-control w-md" />
-          </div></td>   
-            <td>Php 50.00</td>
-            <td>Php 70.00</td>
-          
-               
-          </tr>
-          
-        </tbody>
-      </table>
-      <button type="submit" class="btn btn-default">Reset</button>     <button type="submit" class="btn btn-success">Submit</button> 
-    </div>
-
-                  
-</di
-  </div>
-</div>
-
-
-
-	</div>
-  </div>
+            $items = array_combine($productID,$qtyOrder);
+            $pairs = array();
+            
+            $remarks="n/a";
+            
+            foreach($items as $key=>$value){
+                    $pairs[] = '('.intval($key).','.intval($value).','."'{$_SESSION['username']}'".','."'$total'".','."'$remarks'".')';
+                }
+            
+            require_once('../mysql_connect.php');
+            $query3= "INSERT INTO purchase (productID, purchaseQty, username, totalAmount, remarks) values".implode(',',$pairs);
+            $result3=mysqli_query($dbc,$query3);
+        }
+        
+        ?>
   <!-- /content -->
   
- 
-
-
 
 </div>
 
+<script>
+ $(document).on('click', "#add", function(){
+               var para = document.createElement("tr");
+               var element = document.getElementById("tableList");
+               para.setAttribute("class", "trList");
+               element.appendChild(para);
+               var productChosen = $("#productChosen").find(":selected").text();
+               alert(productChosen);
 
+                $(".trList").append('<td>'+productChosen+'<input type="text" style=display:none readOnly name="productName[]" value="'+productChosen+'"</td>');
+                $(".trList").append('<td><input type="number" min="0" name="orderQty[]" onkeypress="return event.charCode >= 48 && event.charCode <= 57" placeholder="quantity"/></td>');
+                $(".trList").append('<td><input type="button" name="delete" class="ibtnDel btn btn-outline btn-danger"  value=Delete ></td>');
+
+                para.setAttribute("class", "trListSaved");
+            });
+</script>
 </body>
 </html>
