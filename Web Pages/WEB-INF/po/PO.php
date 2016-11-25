09cef500bf072332ed92a51ff6c8d88b5eda399b
Gina Co <gina_co@dlsu.edu.ph>
2016-11-25 22:38:40 +0800

    with CANCEL working!

--- Web Pages/WEB-INF/po/PO.php
+++ Web Pages/WEB-INF/po/PO.php
@@ -89,13 +89,9 @@ $query="Select date(datePurchase) as datePurchase From purchase where poNumber =
         </tr>
       </tbody>
     </table> 
-      
-      <?php
-	  
      
+      <?php
 
-     // $getDate="select date(datePurchase) as datePurchase from purchase where poNumber='{$poNum}';";
-     // $resultDate=mysqli_query($dbc,$query);
 	$query="SELECT ABS(DATEDIFF(datePurchase, CURDATE())) as days FROM purchase WHERE poNumber='{$poNum}';";
 	 
        $result=mysqli_query($dbc,$query);
@@ -105,18 +101,34 @@ $query="Select date(datePurchase) as datePurchase From purchase where poNumber =
 	
           $days=$row["days"];
         }
-       // $datePurchase;
-		
-	
-      //$diff=date_diff($time,$datePurchase);
-     // $diff=$date->diff($datePurchase); 
+        $days;
+
       if($days>3){
-        echo "<button class='btn m-b-xs w-xs btn-danger' disabled>Cancel P.O</button>";
+        echo "<button name=cancel class='btn m-b-xs w-xs btn-danger' disabled>Cancel P.O</button>";
       }else{
-        echo "<button class='btn m-b-xs w-xs btn-danger' >Cancel P.O</button>";
+        echo "<form action='PO.php' method='POST'><button type='hidden-print' name=cancel class='btn m-b-xs w-xs btn-danger' >Cancel P.O</button>
+        <input type=hidden name=poNumber value='$poNum' ></form>";
       }
       ?>
-       
+
+      <?php  
+      if (isset($_POST['cancel'])){
+        $poNumber=$_POST['poNumber'];
+        $deletePO2="DELETE FROM purchase2 where poNumber='{$poNumber}'";
+        $result=mysqli_query($dbc,$deletePO2);
+        echo $deletePO2;
+        echo $deletePO;
+
+
+        $deletePO="DELETE FROM purchase WHERE poNumber='{$poNumber}'";
+        $result=mysqli_query($dbc,$deletePO);
+
+        
+
+        header("location:polist.php"); 
+    exit;
+      }
+      ?>
   </div>
 </div>
 
