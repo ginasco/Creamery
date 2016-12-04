a566adb1f952a37e964e247cfe7597c204a1cd9c
Gina Co <gina_co@dlsu.edu.ph>
2016-12-05 01:43:46 +0800

    fixed

--- Web Pages/WEB-INF/dashboard/homepage_admin.php
+++ Web Pages/WEB-INF/dashboard/homepage_admin.php
@@ -6,7 +6,37 @@
   <meta name="description" content="app, web app, responsive, responsive layout, admin, admin panel, admin dashboard, flat, flat ui, ui kit, AngularJS, ui route, charts, widgets, components" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
+  <style>
 
+    #banner
+    {
+      padding-top: 5em;
+    }
+
+    .wrapper
+    {
+      overflow: hidden;
+      padding: 0em 0em 5em 0em;
+      background: #FFF;
+    }
+    .title
+    {
+      margin-bottom: 3em;
+    }
+    #welcome
+    {
+      overflow: hidden;
+      width: 1000px;
+      padding: 6em 100px 0em 100px;
+      text-align: center;
+    }
+    .container
+    {
+      margin: 0px auto;
+      width: 1200px;
+    }
+
+  </style>
 </head>
 <body>
   <div class="app app-header-fixed ">
@@ -20,44 +50,20 @@
     <!-- content -->
     <div id="content" class="app-content" role="main">
      <div class="app-content-body ">
+       <div class="wrapper">
+        <div id="banner" class="container"><img src="../img/banner.jpg" width="1150" height="500" alt="" /></div>
+        <div id="welcome" class="container">
 
+          <div class="title">
+            <h2>Welcome to Laguna Creamery Inc.</h2>
+          </div>
+          <p> <strong>Your time is limited, so don’t waste it living someone else’s life.</strong> – Steve Jobs</p>
+        </div>
+      </div>
 
-      <div class="hbox hbox-auto-xs hbox-auto-sm" ng-init="
-      app.settings.asideFolded = true; 
-      app.settings.asideDock = true;
-      ">
-	  
-	  
-	  
-	    <div class="col">
-    
-	<h1>
-	<center>
-	<center>Welcome to Dairy MAN </center>
-	<?php echo "Today is " . date("Y/m/d") . "<br>"; 
-	date_default_timezone_set("asia/manila");
-		echo "The time is " . date("h:i:sa");?>
-	
-	</center>
-	</h1>
-  <!-- / main -->
-  <!-- right col -->
-  
-  <!-- / right col -->
-</div>
-
-      <!-- main -->
-   
-<!-- / main -->
-<!-- right col -->
-
-<!-- / right col -->
-</div>
-
-
-</div>
-</div>
-<!-- /content -->
+    </div>
+  </div>  
+  <!-- /content -->
 
 </div>
 </script>
